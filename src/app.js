7e687075dae69bc21930f90a0c189259f9447165
Alvaro Martin Fernandez <91843474+AlvaroMartinFernandez@users.noreply.github.com>
2023-04-27 21:31:32 +0000

    Finalizado con Hack Puedes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,48 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const pronoun = ['the','our'];
+const adj = ['great', 'big' ];
+const noun = ['jogger','racoon','puedes'];
+const ext =['.com','.net','.es','.eu'];
+let domain=[];
+for(let p=0;p<pronoun.length;p++)
+{
+  for(let a=0;a<adj.length;a++)
+  {
+    for(let n=0;n<noun.length;n++)
+    {
+      for(let e=0;e<ext.length;e++){
+        domain.push(pronoun[p]+adj[a]+noun[n]+ext[e]); 
+      }
+    }
+  }
+}
+console.log(domain);
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//hacemos el hack con el noun puedes.
+const hack= (word)=>{
+let wordSlice= word.split(".");
+console.log(wordSlice.slice(-2));
+if( wordSlice[0].slice(-2)=== "es")
+{
+  return wordSlice[0].slice(0,-2)+".es";
+}
+else if( wordSlice[0].slice(-2)== "eu")
+{
+  return wordSlice[0].slice(0,-2)+".eu";
+}
+else if( wordSlice[0].slice(-3)== "com")
+{
+  return wordSlice[0].slice(0,-3)+".com";
+}
+else if( wordSlice[0].slice(-3)== "net")
+{
+  return wordSlice[0].slice(0,-3)+".net";
+}
+else
+{
+return word;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
 };
+console.log(hack("Puedes.Mundo"));
+let hackDomain=domain.map(hack);
+console.log(hackDomain);
