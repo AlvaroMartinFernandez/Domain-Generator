a7740d215794ede5e7b42564b4ede838f136d19a
Alvaro Martin Fernandez <91843474+AlvaroMartinFernandez@users.noreply.github.com>
2023-05-24 09:23:16 +0000

    Hack repair

--- app.js
+++ app.js
@@ -1,48 +1,28 @@
-const pronoun = ['the','our'];
-const adj = ['great', 'big' ];
-const noun = ['jogger','racoon','puedes'];
-const ext =['.com','.net','.es','.eu'];
-let domain=[];
-for(let p=0;p<pronoun.length;p++)
-{
-  for(let a=0;a<adj.length;a++)
-  {
-    for(let n=0;n<noun.length;n++)
-    {
-      for(let e=0;e<ext.length;e++){
-        domain.push(pronoun[p]+adj[a]+noun[n]+ext[e]); 
+const pronoun = ["the", "our"];
+const adj = ["great", "big"];
+const noun = ["jogger", "racoon", "puedes"];
+const ext = [".com", ".net", ".es", ".eu"];
+let domain = [];
+for (let p = 0; p < pronoun.length; p++) {
+  for (let a = 0; a < adj.length; a++) {
+    for (let n = 0; n < noun.length; n++) {
+      for (let e = 0; e < ext.length; e++) {
+        domain.push(pronoun[p] + adj[a] + noun[n] + ext[e]);
       }
     }
   }
 }
 console.log(domain);
 
-//hacemos el hack con el noun puedes.
-const hack= (word)=>{
-let wordSlice= word.split(".");
-console.log(wordSlice.slice(-2));
-if( wordSlice[0].slice(-2)=== "es")
-{
-  return wordSlice[0].slice(0,-2)+".es";
-}
-else if( wordSlice[0].slice(-2)== "eu")
-{
-  return wordSlice[0].slice(0,-2)+".eu";
-}
-else if( wordSlice[0].slice(-3)== "com")
-{
-  return wordSlice[0].slice(0,-3)+".com";
-}
-else if( wordSlice[0].slice(-3)== "net")
-{
-  return wordSlice[0].slice(0,-3)+".net";
-}
-else
-{
-return word;
-}
+//hacemos el hack.
+const hack = (word) => {
+  let wordSlice = word.split(".");
 
+  if (wordSlice[1] === wordSlice[0].slice(-wordSlice[1].length))
+    return wordSlice[0].slice(0, -wordSlice[1].length) + "." + wordSlice[1];
+  else {
+    return word;
+  }
 };
-console.log(hack("Puedes.Mundo"));
-let hackDomain=domain.map(hack);
+let hackDomain = domain.map(hack);
 console.log(hackDomain);
